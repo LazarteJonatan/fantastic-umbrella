d79c05c779f244e4f312ea8cabd43a5f0959cfef
jonatan <jonatan_lazarte1992@hotmail.com>
2023-09-06 15:32:46 -0300

    Primera subida

---
+++ Ejemplo1/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Ejemplo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lwElementos = new System.Windows.Forms.ListView();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.dtpFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.ckbChocolate = new System.Windows.Forms.CheckBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.rbtWizard = new System.Windows.Forms.RadioButton();
+            this.rbtMuggle = new System.Windows.Forms.RadioButton();
+            this.rbtSquibs = new System.Windows.Forms.RadioButton();
+            this.gbxTipos = new System.Windows.Forms.GroupBox();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.cboColorFavorito = new System.Windows.Forms.ComboBox();
+            this.numNumeroFavorito = new System.Windows.Forms.NumericUpDown();
+            this.lblNumeroFavorito = new System.Windows.Forms.Label();
+            this.btnVerPerfil = new System.Windows.Forms.Button();
+            this.gbxTipos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numNumeroFavorito)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(137, 619);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lwElementos
+            // 
+            this.lwElementos.HideSelection = false;
+            this.lwElementos.Location = new System.Drawing.Point(137, 469);
+            this.lwElementos.Name = "lwElementos";
+            this.lwElementos.Size = new System.Drawing.Size(350, 135);
+            this.lwElementos.TabIndex = 7;
+            this.lwElementos.UseCompatibleStateImageBehavior = false;
+            this.lwElementos.View = System.Windows.Forms.View.List;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(287, 109);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(168, 116);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(168, 159);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(90, 13);
+            this.lblFecha.TabIndex = 4;
+            this.lblFecha.Text = "FechaNacimiento";
+            // 
+            // dtpFechaNacimiento
+            // 
+            this.dtpFechaNacimiento.Location = new System.Drawing.Point(287, 159);
+            this.dtpFechaNacimiento.Name = "dtpFechaNacimiento";
+            this.dtpFechaNacimiento.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaNacimiento.TabIndex = 1;
+            // 
+            // ckbChocolate
+            // 
+            this.ckbChocolate.AutoSize = true;
+            this.ckbChocolate.Location = new System.Drawing.Point(287, 199);
+            this.ckbChocolate.Name = "ckbChocolate";
+            this.ckbChocolate.Size = new System.Drawing.Size(135, 17);
+            this.ckbChocolate.TabIndex = 2;
+            this.ckbChocolate.Text = "Te gusta el chocolate?";
+            this.ckbChocolate.UseVisualStyleBackColor = true;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(166, 38);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(165, 29);
+            this.lblTitulo.TabIndex = 7;
+            this.lblTitulo.Text = "Perfil Persona";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rbtWizard
+            // 
+            this.rbtWizard.AutoSize = true;
+            this.rbtWizard.Checked = true;
+            this.rbtWizard.Location = new System.Drawing.Point(6, 36);
+            this.rbtWizard.Name = "rbtWizard";
+            this.rbtWizard.Size = new System.Drawing.Size(58, 17);
+            this.rbtWizard.TabIndex = 8;
+            this.rbtWizard.TabStop = true;
+            this.rbtWizard.Text = "Wizard";
+            this.rbtWizard.UseVisualStyleBackColor = true;
+            // 
+            // rbtMuggle
+            // 
+            this.rbtMuggle.AutoSize = true;
+            this.rbtMuggle.Location = new System.Drawing.Point(123, 36);
+            this.rbtMuggle.Name = "rbtMuggle";
+            this.rbtMuggle.Size = new System.Drawing.Size(60, 17);
+            this.rbtMuggle.TabIndex = 9;
+            this.rbtMuggle.Text = "Muggle";
+            this.rbtMuggle.UseVisualStyleBackColor = true;
+            // 
+            // rbtSquibs
+            // 
+            this.rbtSquibs.AutoSize = true;
+            this.rbtSquibs.Location = new System.Drawing.Point(244, 36);
+            this.rbtSquibs.Name = "rbtSquibs";
+            this.rbtSquibs.Size = new System.Drawing.Size(57, 17);
+            this.rbtSquibs.TabIndex = 10;
+            this.rbtSquibs.Text = "Squibs";
+            this.rbtSquibs.UseVisualStyleBackColor = true;
+            // 
+            // gbxTipos
+            // 
+            this.gbxTipos.Controls.Add(this.rbtSquibs);
+            this.gbxTipos.Controls.Add(this.rbtWizard);
+            this.gbxTipos.Controls.Add(this.rbtMuggle);
+            this.gbxTipos.Location = new System.Drawing.Point(143, 224);
+            this.gbxTipos.Name = "gbxTipos";
+            this.gbxTipos.Size = new System.Drawing.Size(344, 59);
+            this.gbxTipos.TabIndex = 3;
+            this.gbxTipos.TabStop = false;
+            this.gbxTipos.Text = "Tipo";
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(140, 375);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(72, 13);
+            this.lblColor.TabIndex = 12;
+            this.lblColor.Text = "Color Favorito";
+            // 
+            // cboColorFavorito
+            // 
+            this.cboColorFavorito.FormattingEnabled = true;
+            this.cboColorFavorito.Location = new System.Drawing.Point(232, 367);
+            this.cboColorFavorito.Name = "cboColorFavorito";
+            this.cboColorFavorito.Size = new System.Drawing.Size(121, 21);
+            this.cboColorFavorito.TabIndex = 5;
+            // 
+            // numNumeroFavorito
+            // 
+            this.numNumeroFavorito.Location = new System.Drawing.Point(233, 332);
+            this.numNumeroFavorito.Name = "numNumeroFavorito";
+            this.numNumeroFavorito.Size = new System.Drawing.Size(120, 20);
+            this.numNumeroFavorito.TabIndex = 4;
+            // 
+            // lblNumeroFavorito
+            // 
+            this.lblNumeroFavorito.AutoSize = true;
+            this.lblNumeroFavorito.Location = new System.Drawing.Point(137, 338);
+            this.lblNumeroFavorito.Name = "lblNumeroFavorito";
+            this.lblNumeroFavorito.Size = new System.Drawing.Size(85, 13);
+            this.lblNumeroFavorito.TabIndex = 15;
+            this.lblNumeroFavorito.Text = "Numero Favorito";
+            // 
+            // btnVerPerfil
+            // 
+            this.btnVerPerfil.Location = new System.Drawing.Point(143, 428);
+            this.btnVerPerfil.Name = "btnVerPerfil";
+            this.btnVerPerfil.Size = new System.Drawing.Size(75, 23);
+            this.btnVerPerfil.TabIndex = 6;
+            this.btnVerPerfil.Text = "&Ver Perfil";
+            this.btnVerPerfil.UseVisualStyleBackColor = true;
+            this.btnVerPerfil.Click += new System.EventHandler(this.btnVerPerfil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 654);
+            this.Controls.Add(this.btnVerPerfil);
+            this.Controls.Add(this.lblNumeroFavorito);
+            this.Controls.Add(this.numNumeroFavorito);
+            this.Controls.Add(this.cboColorFavorito);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.gbxTipos);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.ckbChocolate);
+            this.Controls.Add(this.dtpFechaNacimiento);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lwElementos);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbxTipos.ResumeLayout(false);
+            this.gbxTipos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numNumeroFavorito)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListView lwElementos;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.DateTimePicker dtpFechaNacimiento;
+        private System.Windows.Forms.CheckBox ckbChocolate;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.RadioButton rbtWizard;
+        private System.Windows.Forms.RadioButton rbtMuggle;
+        private System.Windows.Forms.RadioButton rbtSquibs;
+        private System.Windows.Forms.GroupBox gbxTipos;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.ComboBox cboColorFavorito;
+        private System.Windows.Forms.NumericUpDown numNumeroFavorito;
+        private System.Windows.Forms.Label lblNumeroFavorito;
+        private System.Windows.Forms.Button btnVerPerfil;
+    }
+}
+
---
+++ Ejemplo1/frmPrincipal.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Ejemplo1
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.perfilPersonaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbPerfilPersona = new System.Windows.Forms.ToolStripButton();
+            this.btnBoton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnBoton3 = new System.Windows.Forms.ToolStripButton();
+            this.btnBoton4 = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip2.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.opcionesToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "&Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // opcionesToolStripMenuItem
+            // 
+            this.opcionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.perfilPersonaToolStripMenuItem});
+            this.opcionesToolStripMenuItem.Name = "opcionesToolStripMenuItem";
+            this.opcionesToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.opcionesToolStripMenuItem.Text = "Opci&ones";
+            // 
+            // perfilPersonaToolStripMenuItem
+            // 
+            this.perfilPersonaToolStripMenuItem.Name = "perfilPersonaToolStripMenuItem";
+            this.perfilPersonaToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.perfilPersonaToolStripMenuItem.Text = "Perfil Persona";
+            this.perfilPersonaToolStripMenuItem.Click += new System.EventHandler(this.perfilPersonaToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "Abo&ut";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbPerfilPersona,
+            this.btnBoton2,
+            this.toolStripSeparator1,
+            this.btnBoton3,
+            this.btnBoton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 48);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbPerfilPersona
+            // 
+            this.tsbPerfilPersona.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbPerfilPersona.Image = ((System.Drawing.Image)(resources.GetObject("tsbPerfilPersona.Image")));
+            this.tsbPerfilPersona.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbPerfilPersona.Name = "tsbPerfilPersona";
+            this.tsbPerfilPersona.Size = new System.Drawing.Size(23, 22);
+            this.tsbPerfilPersona.Text = "toolStripButton1";
+            this.tsbPerfilPersona.ToolTipText = "Perfil Persona";
+            this.tsbPerfilPersona.Click += new System.EventHandler(this.tsbPerfilPersona_Click);
+            // 
+            // btnBoton2
+            // 
+            this.btnBoton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnBoton2.Image = ((System.Drawing.Image)(resources.GetObject("btnBoton2.Image")));
+            this.btnBoton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnBoton2.Name = "btnBoton2";
+            this.btnBoton2.Size = new System.Drawing.Size(23, 22);
+            this.btnBoton2.Text = "toolStripButton2";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnBoton3
+            // 
+            this.btnBoton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnBoton3.Image = ((System.Drawing.Image)(resources.GetObject("btnBoton3.Image")));
+            this.btnBoton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnBoton3.Name = "btnBoton3";
+            this.btnBoton3.Size = new System.Drawing.Size(23, 22);
+            this.btnBoton3.Text = "toolStripButton3";
+            // 
+            // btnBoton4
+            // 
+            this.btnBoton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnBoton4.Image = ((System.Drawing.Image)(resources.GetObject("btnBoton4.Image")));
+            this.btnBoton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnBoton4.Name = "btnBoton4";
+            this.btnBoton4.Size = new System.Drawing.Size(23, 22);
+            this.btnBoton4.Text = "toolStripButton4";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmPrincipal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opcionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem perfilPersonaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbPerfilPersona;
+        private System.Windows.Forms.ToolStripButton btnBoton2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnBoton3;
+        private System.Windows.Forms.ToolStripButton btnBoton4;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
